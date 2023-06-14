e0b3bb71628710a4adf5c67be39bc360b25f5d88
Bryunetkin <bryunetkin@gmail.com>
2023-06-14 23:24:25 +0200

    Выполнил второе задание

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,13 @@
 //Функция для проверки длины строки.
 
-const checkStringLength = (string, maxLength) => string.length <= length;
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 checkStringLength("Lake", 3);
 //Функция для проверки, является ли строка палиндромом.
 
 const isPalindrome = (string) => {
-  const normalizedString = string.toLowerCase().replaceAll(" ", "");
-  console.log(checkStringLength);
-  let newString = "";
-  for (let i = normalizedString.length - 1; i >= 0; i--) {
-    newString += normalizedString[i];
-  }
-  return newString === normalizedString;
-  isPalindrome("Лёша на полке клопа нашёл ");
+  string = string.toLowerCase().replaceAll(" ", ""); //Делаем нижний регистр и убираем пробелы
+
+  return string === string.split("").reverse("").join(""); //Сравниваем string with reversed string.Разбиваем строчку на массив и переворачиваем символы в обратном направлении и склеиваем каждый символ
 };
+console.log(isPalindrome("Лёша на полке клопа нашёл "));
