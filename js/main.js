7eb570ee6105efee5503be345a421c0a45059bea
Bryunetkin <bryunetkin@gmail.com>
2023-06-20 22:42:38 +0200

    Work on homework

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-const POSTS_COUNT = 25;
-const MIN_LIKES_COUNT = 15;
-const MAX_LIKES_COUNT = 200;
-const MIN_COUNT_COMMENTS = 0;
-const MAX_COUNT_COMMENTS = 30;
-const MIN_AVATARS_COUNT = 1;
-const MAX_AVATARS_COUNT = 6;
-const DESCRIPTIONS = [];
+const DESCRIPTIONS = [
+  "Мне нужны шестимесячные каникулы дважды в год.",
+  "Жизнь не идеальна, а вот мой лук – да.",
+  "Цени моменты.",
+  "Воскресенье — еще один способ сказать: Какой чудесный день!",
+];
+const NAMES = ["Игнат", "Илларион", "Роза", "Анастасия", "Валера"];
+const MESSAGES = [
+  "Всё отлично!",
+  "В целом всё неплохо. Но не всё.",
+  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
+  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
+];
+
+const POST_COUNTER = 25;
+
+//Функция рандомных чисел
+const getRandomNumber = (min, max) =>
+  Math.gloor(Math.random() * (max - min + 1) + min);
+
+// Функция которая возвращает массив из 25 объектов
+const createPosts = () => {
+  const posts = [];
+
+  return posts;
+};
+
+console.log(createPosts());
